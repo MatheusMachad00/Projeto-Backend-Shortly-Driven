f89ffd03484a94e40800a34c13f72ddce8f64076
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-08-03 11:21:48 -0300

    Feat: Login route finished

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,16 +5,12 @@ import connection from "../dbStrategy/postgres.js";
 export async function userSignup(_, res){
   const signupData = {...res.locals.signupData};
   const encryptedPassword = bcrypt.hashSync(signupData.password, 10);
-/*   console.log(signupData, encryptedPassword) */
   try {
     await connection.query(`
     INSERT INTO users (name, email, password) VALUES ($1, $2, $3)
     `, [signupData.name, signupData.email, encryptedPassword]);
 
-    /* const teste = connection.query("SELECT name FROM users") */
-
     res.sendStatus(201);
-    /* res.send(teste); */
   } catch (error) {
     res.sendStatus(500);
     console.error(error);
@@ -22,5 +18,31 @@ export async function userSignup(_, res){
 }
 
 export async function userLogin (req, res){
+  const loginData = {...res.locals.loginData};
 
+try {
+  const {rows: password} = await connection.query(`
+  SELECT password FROM users WHERE email = ($1)`, [loginData.email]);
+
+  const {rows: id} = await connection.query(`
+  SELECT id FROM users WHERE email = ($1)`, [loginData.email])
+
+  const userId = id[0].id;
+  const userPassword = password[0].password
+
+  if(userId && bcrypt.compareSync(loginData.password, userPassword)){
+    const token = uuid();
+    await connection.query(`
+    DELETE FROM tokens WHERE "userId" = ($1)`, [userId])
+
+    await connection.query(`
+    INSERT INTO tokens ("userId", token) VALUES ($1, $2)`, [userId, token]);
+    return res.status(200).send(token);
+  } else {
+    return res.status(401).send('Senha ou email incorretos!');
+  };
+} catch (error) {
+  res.sendStatus(500);
+    console.error(error);
+}
 }
--- src/middlewares/validateLogin.js
+++ src/middlewares/validateLogin.js
@@ -8,25 +8,7 @@ export async function validateLogin(req, res, next){
   if (validation.error) {
     return res.status(422).send(validation.error.details.map(detail => detail.message))
   };
-
-  const user = await connection.query(`
-  SELECT email FROM users WHERE email = ($1)`, [loginData.email]);
-
-  const password = await connection.query(`
-  SELECT password FROM users WHERE email = ($1)
-  `, [loginData.email]);
-
-  if(user && bcrypt.compareSync(loginData.password, password)){
-    const token = uuid();
-    await connection.query(`
-    INSERT INTO tokens 
-    `)
-    return res.status(201).send(token);
-  } else {
-    return res.status(401).send('Senha ou email incorretos!');
-
-  }
-
+  
   res.locals.loginData = loginData;
   next();
 };
